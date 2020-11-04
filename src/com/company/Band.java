dadc9d677ba7b79f03c043c9c860b286f0382c2f
marcusvdemoura <63955638+marcusvdemoura@users.noreply.github.com>
2020-11-04 13:52:13 +0000

    7th upload

--- src/com/company/Band.java
+++ src/com/company/Band.java
@@ -1,45 +1,53 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Band {
 
     private String name;
     private String style;
+    Musician musician;
 
-    Guitar guitar1 = new Guitar();
-    BaseGuitar baseGuitar1 = new BaseGuitar();
-    Drums drums1 = new Drums();
-    KeyBoard keyBoard1 = new KeyBoard();
-    MusicalNotes musicalNotes = new MusicalNotes();
-    Musician musician1 = new Musician("Jorge", baseGuitar1);
-    Musician musician2 = new Musician("Luke", guitar1);
-    Musician musician3 = new Musician("Ken", drums1);
-    Musician musician4 = new Musician("Louis", keyBoard1);
-    Musician musician5 = new Musician("Kevin", musicalNotes);
+    private HashMap<String, String> listOfMusicians = new HashMap<>();
+//    private ArrayList<Musician> listOfMusicians = new ArrayList<>();
 
 
     public Band(String name, String style) {
-
         this.name = name;
         this.style = style;
+
+    }
+
+    public void bandList(Musician musicianToAdd) {
+        listOfMusicians.put(musician.getMusicianName(), musician.getInstrumentPlayed());
     }
 
     public String getName() {
+
         return name;
     }
 
     public String getStyle() {
+
         return style;
     }
 
-    @Override
-    public String toString() {
-        return String.format("This band is composed by: %s on the %s; %s on the %s; %s on the %s; %s on the %s; %s as the %s", musician1.getMusicianName(),
-                musician1.getInstrumentPlayed(), musician2.getMusicianName(),
-                musician2.getInstrumentPlayed(), musician3.getMusicianName(),
-                musician3.getInstrumentPlayed(), musician4.getMusicianName(),
-                musician4.getInstrumentPlayed(), musician5.getMusicianName(),
-                musician5.getInstrumentPlayed());
+    public HashMap<String, String> getListOfMusicians() {
+        return listOfMusicians;
     }
 
 
+    public void printListMusicians() {
+        Iterator it = getListOfMusicians().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+            it.remove();
+        }
+
+
+    }
 }
--- src/com/company/TestRunner.java
+++ src/com/company/TestRunner.java
@@ -3,14 +3,31 @@ package com.company;
 public class TestRunner {
 
 
+    Guitar guitar1 = new Guitar();
+    BaseGuitar baseGuitar1 = new BaseGuitar();
+    Drums drums1 = new Drums();
+    KeyBoard keyBoard1 = new KeyBoard();
+    MusicalNotes musicalNotes = new MusicalNotes();
+    Musician musician1 = new Musician("Jorge", baseGuitar1);
+    Musician musician2 = new Musician("Luke", guitar1);
+    Musician musician3 = new Musician("Ken", drums1);
+    Musician musician4 = new Musician("Louis", keyBoard1);
+    Musician musician5 = new Musician("Kevin", musicalNotes);
     Band band1 = new Band("Hallo Maters", "Rock");
     Orchestra orchestra1 = new Orchestra("National Orchestra", "Chamber");
 
+
+
     public void Run() {
 
-        System.out.println(band1.toString());
-        System.out.println(orchestra1.toString());
-        System.out.println(band1.musician1.getSound());
+        band1.bandList(musician1);
+        band1.bandList(musician2);
+        band1.bandList(musician3);
+        band1.bandList(musician4);
+        band1.bandList(musician5);
+
+        band1.printListMusicians(); //I'm not sure how to print
+        // the hashmap with both key (name of the musician) and value (instrument played)...
 
 
     }
